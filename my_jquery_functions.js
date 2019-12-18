3a2aae6ce4a16d311f1db6f61aa5808e5cb15a88
Isabella O'Duffy <ioduffy@yahoo.co.uk>
2019-12-18 11:03:56 +0000

    Final

---
+++ my_jquery_functions.js
@@ -0,0 +1,12 @@
+$('.carousel').carousel({
+  interval: 6000,
+  pause: "false"
+});
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
